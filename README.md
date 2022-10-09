05d48347d7a3b02f5e65407cc6d483c10084f909
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-10-09 22:32:56 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,30 @@
+# kcrypt-challenger
+
+| :exclamation: | This is experimental! |
+|-|:-|
+
+This is the Kairos kcrypt-challenger Kubernetes Native Extension. 
+
+To install, use helm:
+
+```
+# Adds the kairos repo to helm
+$ helm repo add kairos https://kairos-io.github.io/helm-charts
+"kairos" has been added to your repositories
+$ helm repo update                                        
+Hang tight while we grab the latest from your chart repositories...
+...Successfully got an update from the "kairos" chart repository
+Update Complete. ⎈Happy Helming!⎈
+
+# Install the CRD chart
+$ helm install kairos-crd kairos/kairos-crds
+NAME: kairos-crd
+LAST DEPLOYED: Tue Sep  6 20:35:34 2022
+NAMESPACE: default
+STATUS: deployed
+REVISION: 1
+TEST SUITE: None
+
+# Installs challenger
+$ helm install kairos-challenger kairos/challenger
+```
---
+++ cmd/discovery/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/jaypipes/ghw/pkg/block"
+	"github.com/kairos-io/go-tpm"
+	"github.com/kairos-io/kairos/pkg/machine"
+	"github.com/kairos-io/kcrypt/pkg/bus"
+	"gopkg.in/yaml.v3"
+
+	"github.com/mudler/go-pluggable"
+)
+
+func main() {
+	if len(os.Args) >= 2 && bus.IsEventDefined(os.Args[1]) {
+		checkErr(start())
+	}
+
+	pubhash, _ := tpm.GetPubHash()
+	fmt.Print(pubhash)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+// ❯ echo '{ "data": "{ \\"label\\": \\"LABEL\\" }"}' | sudo -E WSS_SERVER="http://localhost:8082/challenge" ./challenger "discovery.password"
+func start() error {
+	factory := pluggable.NewPluginFactory()
+
+	connectionDetails := &struct {
+		Server string
+	}{}
+
+	var server string
+	d, err := machine.DotToYAML("/proc/cmdline")
+	if err == nil { // best-effort
+		yaml.Unmarshal(d, connectionDetails) //nolint:errcheck
+	}
+	server = connectionDetails.Server
+	if os.Getenv("WSS_SERVER") != "" {
+		server = os.Getenv("WSS_SERVER")
+	}
+
+	// Input: bus.EventInstallPayload
+	// Expected output: map[string]string{}
+	factory.Add(bus.EventDiscoveryPassword, func(e *pluggable.Event) pluggable.EventResponse {
+		b := &block.Partition{}
+		err := json.Unmarshal([]byte(e.Data), b)
+		if err != nil {
+			return pluggable.EventResponse{
+				Error: fmt.Sprintf("failed reading partitions: %s", err.Error()),
+			}
+		}
+
+		msg, err := tpm.Get(server, tpm.WithAdditionalHeader("label", b.Label))
+		if err != nil {
+			return pluggable.EventResponse{
+				Error: fmt.Sprintf("failed contacting from wss server: %s", err.Error()),
+			}
+		}
+		result := map[string]interface{}{}
+		err = json.Unmarshal(msg, &result)
+		if err != nil {
+			return pluggable.EventResponse{
+				Error: fmt.Sprintf("failed reading from wss server: %s", err.Error()),
+			}
+		}
+		p, ok := result["passphrase"]
+		if !ok {
+			return pluggable.EventResponse{
+				Error: "not found",
+			}
+		}
+
+		return pluggable.EventResponse{
+			Data: fmt.Sprint(p),
+		}
+	})
+
+	return factory.Run(pluggable.EventType(os.Args[1]), os.Stdin, os.Stdout)
+}
